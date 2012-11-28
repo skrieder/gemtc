dd8f41ce3b4cf4afc107d54b8273cb79d0ed38b6
Scott Krieder <skrieder@iit.edu>
2012-11-28 16:07:38 -0600

    adding swift big sleep test

---
+++ Tests/swift-sleep-tests/589-big-loop.swift
@@ -0,0 +1,28 @@
+
+/*
+  Reported to exm-user by Scott Krieder on 10/24/2012
+  Cannot reproduce problem. -Justin
+*/
+
+#include <builtins.swift>
+#include <io.swift>
+#include <sys.swift>
+
+main
+{
+     int bound = toint(argv("bound"));
+     float sleepTime = tofloat(argv("sleeptime"));
+
+     // int bound = 1;
+     // float sleepTime = 0;
+
+     // print for debug
+     printf("The number of arguments is: %i\n", argc());
+     printf("The bound is: %i\n", bound);
+     printf("The sleeptime is: %f\n", sleepTime);
+
+     // run the sleep
+     foreach i in [1:bound:1]{
+               sleep(sleepTime);
+     }
+}
---
+++ Tests/swift-sleep-tests/test.sh
@@ -0,0 +1,1 @@
+/usr/bin/time -f %e turbine -n 3 589-big-loop.tcl -bound=100000 -sleeptime=0
