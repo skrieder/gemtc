3ff991a90aad01df9ffba6e6bb31e7bc14359005
Scott Krieder <skrieder@iit.edu>
2012-11-30 12:43:58 -0600

    updating with some fake sleeps

--- dummyAPI/api.c
+++ dummyAPI/api.c
@@ -1,20 +1,30 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "api.h"
 
 void gemtcSetup(){
 
-  printf("In setup\n");
+  printf("In setup.\n");
+
+  // sleep to mimic overhead
+  sleep(1);
 
 }
 
 void gemtcRun(){
 
-  printf("In run\n");
+  printf("In run.\n");
+
+  // sleep to mimic overhead
+  sleep(1);
 
 }
 
 void gemtcCleanup(){
 
-  printf("In cleanup\n");
+  printf("In cleanup.\n");
+  
+  // sleep to mimic overhead
+  sleep(3);
 
 }
