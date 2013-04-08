8c13e230d97315cbf1e5df7d29f14108fd7a8c43
Dustin Shahidehpour <dshahidehpour@gmail.com>
2013-04-07 22:24:14 -0500

    successfully added arrays to params

--- Kernels/Fake.cu
+++ Kernels/Fake.cu
@@ -2,6 +2,14 @@ __device__ void Fake(void *params){
   int *np = (int*)params;
   int *nb = np + 1;
 
+  int size = (*np) * (*nb); 
+
+  double *pos = (double *)(np + 2);
   *np = 15;
   *nb = 68;
+  
+  int i;
+  for(i=0; i<size; i++){
+    pos[i] = i; 
+  }
 }
--- Tests/mdproxy/ComputeTest.cu
+++ Tests/mdproxy/ComputeTest.cu
@@ -6,16 +6,20 @@
 int main(int argc, char **argv){
   gemtcSetup(25600, 0);
   
-  int size = sizeof(int)*2;
+  int np = 5;
+  int nb = 6;
+  int a_size = np * nb;
+
+  int size = sizeof(int)*2 + sizeof(double)*a_size;
   
   void *d_mem = gemtcGPUMalloc(size);
   void *h_mem = malloc(size);
 
-  int np = 5;
-  int nb = 6;
-  
+  double pos[a_size];
+
   memcpy(h_mem, &np, sizeof(int));
   memcpy((((int*)h_mem)+1), &nb, sizeof(int));
+  memcpy((((int*)h_mem)+2), pos, sizeof(double)*a_size); 
 
   gemtcMemcpyHostToDevice(d_mem, h_mem, size);
   gemtcPush(16, 1, 12000, d_mem); 
@@ -31,8 +35,13 @@ int main(int argc, char **argv){
 
   int *p_np = (int*)results;
   int *p_nb = ((int*)results)+1;
-  
+  double *p_pos = (double*)((int*)results + 2); 
+
   printf("np is : %d\nnb is : %d\n", *p_np, *p_nb);
+  int i;
+  for(i=0; i<a_size; i++){
+    printf("%f\n", p_pos[i]);
+  }
 
   gemtcGPUFree(results);
   ret = NULL;
