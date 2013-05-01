aa526c732fc4c9564212d93c7bfa44ad3fd1c527
Dustin Shahidehpour <dshahidehpour@gmail.com>
2013-05-01 15:56:36 -0500

    Created Table Struct to make unpacking less stressful for Single Node

--- Kernels/MDProxy.cu
+++ Kernels/MDProxy.cu
@@ -1,83 +1,93 @@
+typedef struct 
+{
+  int np, nd;
+  double *mass;
+  double *pos, *vel, *acc, *f, *pe, *ke;
+}Table;
+
+__device__ Table Unpack_Table(void *params){
+  
+  Table data; 
+
+  data.np = *((int*) params);
+  data.nd = *(((int*) params)+1);
+
+  int size = data.np * data.nd;
+
+  data.mass = (double*)(((int*)params)+2);
+  data.pos = data.mass + 1;
+  data.vel = data.pos + size;
+  data.acc = data.vel + size;
+  data.f = data.acc + size;
+
+  data.pe = data.f + size;
+  data.ke = data.pe + size;
+
+  return data;
+}
+
 ///////////////////////////////////////////////////
 //////////// Single Node Kernels ///////////////////
 ////////////////////////////////////////////////////
-
 __device__ void ComputeParticles(void* params){  
   
   //Params| &table | offset | 
   //Bytes |   8    |   4    | 
-  
- void *table = *((void**)params);
- int offset = *((int*)(((void**)params)+1));
-  
-  //Extract all the values. 
-  int np = *((int*) table);
-  int nd = *(((int*) table)+1);
 
-  int size = np * nd;
+  void *table = *((void**)params);
+  Table dt = Unpack_Table(table);
 
-  double *mass = (double*)(((int*)table)+2);
-  double *pos = mass + 1;
-  double *vel = pos + size; 
-  double *acc = vel + size;
-  double *f = acc + size;
-
-  double *pe = f + size;
-  double *ke = pe + size;
+  int offset = *((int*)(((void**)params)+1));
 
   int i, j;
-
   double d, d2; 
   double PI2 = 3.141592653589793 / 2.0;
   double rij[3];
- 
+
   int tid = threadIdx.x % 32; 
   int k = offset + tid; 
   //Compute all the potential energy and forces.
-      for(i=0; i<nd; i++){
-        f[i+k*nd] = 0.0;
-      }
-     
-      for(j=0; j<np; j++){
-        if(k == j){ continue; }
+  for(i=0; i<dt.nd; i++){
+    dt.f[i+k*dt.nd] = 0.0;
+  }
 
-        d = 0.0; 
-        for(i=0; i<nd; i++){
-          rij[i] = pos[k*nd+i] - pos[j*nd+i];
-          d += pow(rij[i], 2); 
-          d = rij[i];
-        }
+  for(j=0; j<dt.np; j++){
+    if(k == j){ continue; }
 
-        d = sqrt(d); 
-        d2 = d < PI2? d : PI2; 
+    d = 0.0; 
+    for(i=0; i<dt.nd; i++){
+      rij[i] = dt.pos[k*dt.nd+i] - dt.pos[j*dt.nd+i];
+      d += pow(rij[i], 2); 
+      d = rij[i];
+    }
 
-        pe[k] +=  0.5 * pow(sin(d2), 2);
-        
-        for(i=0; i<nd; i++){
-          f[i+k*nd] -= rij[i] * sin(2.0 * d2) / d;
-        }
-      }
+    d = sqrt(d); 
+    d2 = d < PI2? d : PI2; 
 
-      for(i=0; i < nd; i++){
-        ke[k] += vel[i+k*nd] * vel[i+k*nd];
-      }
+    dt.pe[k] +=  0.5 * pow(sin(d2), 2);
+    
+    for(i=0; i<dt.nd; i++){
+      dt.f[i+k*dt.nd] -= rij[i] * sin(2.0 * d2) / d;
+    }
+  }
 
-      ke[k] *= 0.5 * (*mass);
+  for(i=0; i < dt.nd; i++){
+    dt.ke[k] += dt.vel[i+k*dt.nd] * dt.vel[i+k*dt.nd];
+  }
+
+  dt.ke[k] *= 0.5 * (*dt.mass);
 }
 
 __device__ double r8_uniform_01(int *seed){
-  int k; 
-  double r;
 
-  k = *seed/ 127773;
+  int k = *seed/ 127773;
   *seed = 16807 * (*seed - k * 127773) - k * 2836;
 
   if( *seed < 0 ){
     *seed += 2147483647;
   }
 
-  r = (double)(*seed) * 4.656612875E-10;
-  return r; 
+  return (double)(*seed) * 4.656612875E-10; 
 }
 
 __device__ void InitParticles(void* params){
@@ -85,22 +95,18 @@ __device__ void InitParticles(void* params){
   //Params| &table | box[] | seed |
   //Bytes |   8    | 8*nd  |  4   |
   
-  void *table = *((void**)params); 
+  void *table = *((void**)params);
+  Table dt = Unpack_Table(table); 
 
-  //Unpack Table  
-  int np = *((int*)table);
-  int nd = *(((int*)table) + 1);
-  double *pos = ((double*)table) + 2;
- 
   //Unpack Params
   double *box = (double*)(((void**)params)+1);
-  int *seed = (int*)(box + nd);
+  int *seed = (int*)(box + dt.nd);
 
   int i,j; 
   //Update values
-  for ( j = 0; j < np ; j++){
-    for ( i = 0; i < nd ; i++){
-      pos[i+j*nd] = box[i] * r8_uniform_01(seed);
+  for ( j = 0; j < dt.np ; j++){
+    for ( i = 0; i < dt.nd ; i++){
+      dt.pos[i+j*dt.nd] = box[i] * r8_uniform_01(seed);
     }
   }
 }
@@ -111,38 +117,25 @@ __device__ void UpdatePosVelAccel(void* params){
   //Bytes:  |    8   |   8  |   4    |
 
   void *table = *((void**)params);
+  Table t = Unpack_Table(table);
+
   double dt = *((double*)(((void**)params)+1));
   int offset = *(((int*)params) + 4);
   
-  //Unpack Table
-  int np = *((int*)table);
-  int nd = *(((int*)table) + 1);
-  
-  int size = np * nd;
-
-  double mass = *(((double*)table) + 1); 
-   
-  double *pos = ((double*)table) + 2;
-  double *vel = pos + size;
-  double *acc = vel + size; 
-  double *f = acc + size; 
-  double *pe = f + size;
-  double *ke = pe + size; 
-  
   int i,j;
-  double rmass = 1.0 / mass;
+  double rmass = 1.0 / (*t.mass);
 
   int tid = threadIdx.x % 32;
   j = offset + tid; 
 
   //Begin computation
-  for ( i = 0 ; i < nd; i ++){
-    pos[i+j*nd] += vel[i+j*nd] * dt + 0.5 * acc[i+j*nd] * dt * dt;   
-    vel[i+j*nd] += 0.5 * dt * (f[i+j*nd] * rmass + acc[i+j*nd]);
-    acc[i+j*nd] = f[i+j*nd] * rmass;
+  for ( i = 0 ; i < t.nd; i ++){
+    t.pos[i+j*t.nd] += t.vel[i+j*t.nd] * dt + 0.5 * t.acc[i+j*t.nd] * dt * dt;   
+    t.vel[i+j*t.nd] += 0.5 * dt * (t.f[i+j*t.nd] * rmass + t.acc[i+j*t.nd]);
+    t.acc[i+j*t.nd] = t.f[i+j*t.nd] * rmass;
     
-    pe[i+j*nd] = 0.0;
-    ke[i+j*nd] = 0.0;
+    t.pe[i+j*t.nd] = 0.0;
+    t.ke[i+j*t.nd] = 0.0;
   }
 }
 
