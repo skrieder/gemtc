ee25ea04cbd077f8279aa93e2812b117c3b1707e
Scott Krieder <skrieder@iit.edu>
2013-05-21 09:58:39 -0500

    adding simple size of test

---
+++ Tests/c-tests/sizeof.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Int: %d\n", sizeof(int));
+  printf("Long Int: %d\n", sizeof(long int));
+  printf("Double: %d\n", sizeof(double));
+  printf("Float: %d\n", sizeof(float));
+}
