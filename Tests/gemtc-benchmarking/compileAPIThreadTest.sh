c7786a650efd54af889d16682159b7fc8ca9ae36
Scott Krieder <skrieder@iit.edu>
2013-10-10 15:55:23 -0500

    remove test prints

--- Tests/gemtc-benchmarking/compileAPIThreadTest.sh
+++ Tests/gemtc-benchmarking/compileAPIThreadTest.sh
@@ -1,13 +1,3 @@
 #!/bin/sh
 /usr/local/cuda-5.0/bin/nvcc -arch=sm_11  APIThreadTest.cu -o ../../bin/APIThread
-echo "Testing!!!!"
-echo "Testing!!!!"
-echo "Testing!!!!"
-echo "Testing!!!!"
-echo "Testing!!!!"
-echo "Testing!!!!"
-echo "Testing!!!!"
-echo "Testing!!!!"
-echo "Testing!!!!"
-echo "Testing!!!!"
 exit
