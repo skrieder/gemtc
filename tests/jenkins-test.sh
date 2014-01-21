e0ade092a6fc3c719695c60ee1ae99b21c4bbe9a
Scott Krieder <skrieder@iit.edu>
2014-01-21 14:38:29 -0600

    Adding a jenkins-test file. Rather than writing tests line by line in Jenkins interface, jenkins will call this one script to execute all the tests. Code is still todo.

---
+++ tests/jenkins-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This file will be run nightly by Jenkins.
+
+# FORALL folders in this dir
+
+  # compile code in subdir
+
+  # run tests in subdir
+
+# Exit to satisfy Jenkins, else build will fail
+exit
