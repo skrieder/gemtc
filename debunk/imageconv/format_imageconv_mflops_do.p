7e9f3b3fd24886e709cc9f9317e919f85185068e
karthikb87 <karthikb87@gmail.com>
2014-02-10 17:09:00 -0600

    Changed to MFlops to GFlops

--- debunk/imageconv/format_imageconv_mflops_do.p
+++ debunk/imageconv/format_imageconv_mflops_do.p
@@ -15,14 +15,14 @@ set format y "%'g"
 
 # set labels
 unset label
-set title "Image Convolution MFLOPS with Varied Input and Thread Count"
+set title "Image Convolution MFLOPS with Varied Input and Thread Count - Data Omitted"
 set xlabel "Total Problem Size (bytes)"
 set ylabel "GFLOPS"
 set key outside
 
 # set output file
 set terminal png size 1600,900 enhanced font "Helvetica,20"
-   set output 'plots/imageconv_mflops_data_incl_460.png'
+   set output 'plots/imageconv_mflops_data_incl_460_onlygpu.png'
 
 # plot data
 plot "logs/imageconv.dat" using 1:2 title '1 Thread' w linespoints, \
--- debunk/imageconv/imageconv.sh
+++ debunk/imageconv/imageconv.sh
@@ -5,11 +5,11 @@
 # Illinois Institute of Technology
 TMP_DIR=$PWD
 image_width=100
-mask_width=5
-FLAG=1
+mask_width=50
+FLAG=$1
 echo "Image width in 1-D: $image_width"
 echo "Mask width: $mask_width"
-total_problem_size=$(($image_width*2+$mask_width))
+total_problem_size=$(($image_width+$mask_width))
 echo "Total Problem Size (array elements): $total_problem_size"
 total_problem_size_bytes=$(($total_problem_size*4)) # 4 because 4 bytes in a float
 echo "Total Problem Size (bytes): $total_problem_size_bytes"
@@ -42,13 +42,17 @@ do
     # print a new line
     printf "\n" >> logs/imageconv.dat
     image_width=$(($image_width*2))
-    total_problem_size=$(($image_width*2+$mask_width))
+    total_problem_size=$(($image_width+$mask_width))
     total_problem_size_bytes=$(($total_problem_size*4))
 done
 
-gnuplot format_imageconv_mflops.p
-scp plots/imageconv_mflops_data_incl_460.png karthik@datasys.cs.iit.edu:~/public_html/
-scp plots/imageconv_mflops_data_incl_460_onlygpu.png karthik@datasys.cs.iit.edu:~/public_html/
-
+if [$FLAG -eq 1]
+then
+  gnuplot format_imageconv_mflops.p
+  scp plots/imageconv_mflops_data_incl_460.png karthik@datasys.cs.iit.edu:~/public_html/
+else
+  gnuplot format_imageconv_mflops_do.p
+  scp plots/imageconv_mflops_data_incl_460_onlygpu.png karthik@datasys.cs.iit.edu:~/public_html/
+fi
 cd $TMP_DIR
 exit
