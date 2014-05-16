4bbebbe5f054db9456d2c65d3e834c1f3d923252
Karthik <karthikb87@gmail.com>
2014-05-15 22:34:43 -0500

    modiyin raven

--- debunk/hist/raven/gemtc/compileScript.sh
+++ debunk/hist/raven/gemtc/compileScript.sh
@@ -3,12 +3,12 @@
 # Karthik Balasubramanian
 # kbalasu3@hawk.iit.edu
 # Illinois Institute of Technology
-
+make
 ./histogram 5 1 > /lus/scratch/p01956/histogram.dat
 mv /lus/scratch/p01956/histogram.dat histogram.dat
 gnuplot format_histogram_time_raven.p
 gnuplot format_histogram_thrgh_raven.p
 scp plots/hist_throughput_raven.png karthik@datasys.cs.iit.edu:/home/karthik/public_html/hist_throughput_raven.png
 scp plots/hist_time_raven.png karthik@datasys.cs.iit.edu:/home/karthik/public_html/hist_time_raven.png
-rm histogram
+make clean
 rm GeM*
