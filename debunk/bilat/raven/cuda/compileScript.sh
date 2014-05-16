638ca8c118637f69dcd376a8a58f09f1c2dc44e7
Karthik <karthikb87@gmail.com>
2014-05-16 01:20:20 -0500

    Upated for raven

--- debunk/bilat/raven/cuda/compileScript.sh
+++ debunk/bilat/raven/cuda/compileScript.sh
@@ -3,9 +3,46 @@
 # Karthik Balasubramanian
 # kbalasu3@hawk.iit.edu
 # Illinois Institute of Technology
+TMP_DIR=$PWD
+WIDTH=1024
+HEIGHT=1024
+NUM_TASKS=168
+LOG_DIR="/lus/scratch/p01956/filtering.dat"
+echo "Width : $WIDTH Height: $HEIGHT"
+WIDTH=$(($WIDTH/NUM_TASKS))
+total_problem_size=$(($WIDTH * $HEIGHT))
+echo "Total Problem Size (array elements): $total_problem_size"
+total_problem_size_bytes=$(($total_problem_size*12)) # 4 because 4 bytes in a float
+echo "Total Problem Size (bytes): $total_problem_size_bytes"
 
-mv /lus/scratch/p01956/filtering.dat filtering.dat
+cd $TMP_DIR
+make
+max=6
+# loop over Problem size
+printf "\n" > $LOG_DIR
+
+for j in $(seq 1 $max) 
+do
+    threads=1
+    printf "$total_problem_size_bytes\t" >> $LOG_DIR
+		# Loop over threads
+		for i in {1..11} 
+		do
+	          ./filtering $WIDTH $HEIGHT $threads >> $LOG_DIR
+		   threads=$(($threads*2))
+		done
+    # print a new line
+    printf "\n" >> $LOG_DIR
+    WIDTH=$(($WIDTH*2))
+    total_problem_size=$(($WIDTH * $HEIGHT))
+    total_problem_size_bytes=$(($total_problem_size*12))
+done
+
+#mv $LOG_DIR $TEMP_LOG_DIR
+cd $TMP_DIR
+
+mv $LOG_DIR filtering.dat
 gnuplot format_bilat_time_raven.p
 scp plots/bilat_time_raven.png karthik@datasys.cs.iit.edu:/home/karthik/public_html/bilat_time_raven.png
-rm filtering
+make clean
 rm GeMTC*
--- debunk/bilat/raven/cuda/execute.sh
+++ debunk/bilat/raven/cuda/execute.sh
@@ -3,5 +3,4 @@
 # Karthik Balasubramanian
 # kbalasu3@hawk.iit.edu
 # Illinois Institute of Technology
-
-./filtering 8 10 > /lus/scratch/p01956/filtering.dat
+./compileScript.sh
